8e616ec513a7d5d4c338e14648dc94e16f265125
Maylis Maria <maylis.maria15@gmail.com>
2023-11-14 16:45:41 -0300

    Postagem

--- blogpessoal/src/services/Service.ts
+++ blogpessoal/src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://db-blogpesoal-clvr.onrender.com'
+    baseURL: 'https://blogpessoal-u2pw.onrender.com'
 })
 
 export const login = async (url: string, dados: Object, setDados: Function) => {
